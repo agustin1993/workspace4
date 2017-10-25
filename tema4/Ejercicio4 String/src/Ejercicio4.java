7eca778af144fd65d255bd05905d152ab2ca7516
Agustin Lopez <lopezagustinsmr@gmail.com>
2017-10-25 21:11:16 +0200

    tema4

--- tema4/Ejercicio4 String/src/Ejercicio4.java
+++ tema4/Ejercicio4 String/src/Ejercicio4.java
@@ -1,9 +1,33 @@
-
+import java.util.*;
 public class Ejercicio4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String frase;
+		int opcion;
+		final int MAX_LONGITUD = 80;
+		
+		Scanner teclado = new Scanner(System.in);
+		do{
+			System.out.println("Frase de hasta "+ MAX_LONGITUD +" caracteres: ");
+			frase = teclado.nextLine();
+		}while(frase.length() > MAX_LONGITUD);
+		
+		System.out.println("Elija una opcion para el texto:");
+		System.out.println("1. Mayusculas");
+		System.out.println("2. Minusculas");
+		opcion = teclado.nextInt();
+		while (opcion < 1  ||  opcion > 2){
+			System.out.println("Elija una opcion para el texto:");
+			System.out.println("1. Mayusculas");
+			System.out.println("2. Minusculas");
+			opcion = teclado.nextInt();
+		}
+		if(opcion == 1){
+			System.out.println(frase.toUpperCase());
+		}else{
+			System.out.println(frase.toLowerCase());
+		}
+		
 	}
 
 }
