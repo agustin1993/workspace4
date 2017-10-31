6b7029af27395e084ee806e3cc886a7bb4329ae9
Agustin Lopez <lopezagustinsmr@gmail.com>
2017-10-31 17:22:51 +0100

    vectores

--- tema4/Ejercicio10 String/src/Ejercicio10.java
+++ tema4/Ejercicio10 String/src/Ejercicio10.java
@@ -5,8 +5,8 @@ public class Ejercicio10 {
 	public static void main(String[] args) {
 		String frase;
 		final int max_length=60;
-		int letras = 0;
-		int frecuencia=0;
+		int v [] = new int [26];
+		int k;
 		
 		Scanner teclado = new Scanner(System.in);
 		do{
@@ -14,9 +14,13 @@ public class Ejercicio10 {
 			frase=teclado.nextLine();
 		}while (frase.length()>max_length);
 		
-		String mayusculas = frase.toUpperCase();
+		frase = frase.toUpperCase();
 		
-		
+		for(k = 0;k<=frase.length(); k++){
+			if(frase.charAt(k) < v [26]){
+				
+			}
+		}
 	}
 
 }
